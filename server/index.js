127818b4676ff6258629a05f7b12f53362bc32aa
Blake Crawford <blake.crawford@outlook.com>
2022-10-01 13:00:22 -0700

    added auth check middleware to verify all requests contain authorization

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+const authChecker = require("./middleware/authChecker");
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(authChecker);
 
 // ---- PRODUCT OVERVIEW ROUTES ---- //
 

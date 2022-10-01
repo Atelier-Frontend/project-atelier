0e61183d9fa0d0c1dab139b4cf860629cba53495
Chuck Song <chuckthedeveloper@gmail.com>
2022-10-01 15:41:24 -0400

    Complete simple index_jsx

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (<div>
+      <h1>Project Atelier</h1>
+    </div>)
+  }
+}
+
+  ReactDOM.render(<App />, document.getElementById('app'));

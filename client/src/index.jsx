f8db4fa5668aa9b1ce214209fa1dda99db6b52e2
Blake Crawford <blake.crawford@outlook.com>
2022-10-02 20:34:16 -0700

    Completed unit tests for Product Overview API calls and added additional chai functionality

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -23,6 +23,7 @@ class App extends React.Component {
   }
 
   render () {
+    console.log(this.state);
     return (<div>
       <h1>Project Atelier</h1>
       {this.state.products.length > 0 && 

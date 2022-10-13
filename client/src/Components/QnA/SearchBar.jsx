3373e8a12c4321386b448387c768c877bd426890
Chuck Song <chuckthedeveloper@gmail.com>
2022-10-13 01:29:22 -0400

    Implemented Search_Questions feature

--- client/src/Components/QnA/SearchBar.jsx
+++ client/src/Components/QnA/SearchBar.jsx
@@ -6,13 +6,26 @@ class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      term: ""
+    }
+  }
+
+  questionSearch (e) {
+    this.setState({
+      term: e.target.value
+    })
+    if(e.target.value.length >= 3) {
+      this.props.filterQuestions(e.target.value)
+    } else {
+      this.props.getQList()
     }
   }
 
   render() {
     return (<div className="answer-search-field">
       <input placeholder={"Have a question? Search for answers…"}
-             style={{width: "50vw", height: "3vh"}}/>
+             style={{width: "50vw", height: "3vh"}}
+             onChange={this.questionSearch.bind(this)} />
       <button className="search-button">
         <img src={icon}></img>
       </button>

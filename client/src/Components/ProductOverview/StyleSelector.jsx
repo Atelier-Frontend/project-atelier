99c38ff51459e6e7ca32665930826328fc0b5b19
Blake Crawford <blake.crawford@outlook.com>
2022-10-21 00:06:26 -0700

    added further styling

--- client/src/Components/ProductOverview/StyleSelector.jsx
+++ client/src/Components/ProductOverview/StyleSelector.jsx
@@ -29,7 +29,7 @@ class StyleSelector extends React.Component {
 
   render() {
     return (<div className='style-container'>
-      <span className='style-info'>STYLE > </span>
+      <span className='style-info'>STYLE ></span>
       <span className='style-name'>{this.state.name}</span>
       <div className='style-gallery'>
         {this.props.styles.map((style, index) => (

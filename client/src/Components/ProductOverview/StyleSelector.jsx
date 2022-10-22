7f29f56e6e9fd00381bcf05a8836049b8f6007d9
Blake Crawford <blake.crawford@outlook.com>
2022-10-21 21:11:40 -0700

    selected style is now highlighted

--- client/src/Components/ProductOverview/StyleSelector.jsx
+++ client/src/Components/ProductOverview/StyleSelector.jsx
@@ -11,31 +11,36 @@ class StyleSelector extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ name: this.props.selectedStyle.name.toUpperCase() });
+    this.setState({ 
+      name: this.props.selectedStyle.name.toUpperCase(), 
+      item: this.props.styles[0].name
+    });
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.selectedStyle.name !== prevProps.selectedStyle.name) {
-      this.setState({ name: this.props.selectedStyle.name.toUpperCase() });
+      this.setState({ name: this.props.selectedStyle.name.toUpperCase()});
+    }
+    if (this.props.styles !== prevProps.styles) {
+      this.setState({ item: this.props.styles[0].name});
     }
   }
 
   onClick(event) {
     event.preventDefault();
-    console.log(event.target.classList)
-    event.target.classList.toggle('selected');
-    this.setState({ clicked: true });
+    this.setState({ item: event.target.id });
     this.props.selectImage(event.target.id);
     return false;
   }
 
   render() {
+    console.log(this.props)
     return (<div className='style-container'>
       <span className='style-info'>STYLE ></span>
       <span className='style-name'>{this.state.name}</span>
       <div className='style-gallery'>
         {this.props.styles.map((style, index) => (
-            <img className='style-image' alt='image in style gallery' key={index} id={style.name} src={style.photos[0].thumbnail_url} onClick={this.onClick} />
+            <img className={this.state.item === style.name ? 'selected' : 'style-image'} alt='image in style gallery' key={index} id={style.name} src={style.photos[0].thumbnail_url} onClick={this.onClick} />
         ))}
       </div>
     </div>)

04ebb4290d3a263e015f476f3f770531daaec35c
Matt Dailey <mattdailey@matts-mbp.lan>
2022-10-22 15:45:56 -0500

    CSS Improvement

---
+++ client/src/Components/Clicked.js
@@ -0,0 +1,24 @@
+import react from 'react'
+import axios from 'axios'
+
+
+
+
+function clicked (comp, el) {
+  var test = {
+    widget: comp,
+    element: el,
+    time: JSON.stringify(Date.now())
+  }
+  axios.post('/interactions', test)
+  .then((response)=> {
+    console.log(response, ' Response');
+  })
+  .catch((err)=> {
+    console.log(err);
+  })
+
+}
+
+
+export default clicked;

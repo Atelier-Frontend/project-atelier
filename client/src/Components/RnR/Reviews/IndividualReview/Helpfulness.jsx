2acf6b91f929189d105b86b93b8b23893a8122b4
Chuck Song <chuckthedeveloper@gmail.com>
2022-10-22 15:39:05 -0400

    Implemented report review feature

--- client/src/Components/RnR/Reviews/IndividualReview/Helpfulness.jsx
+++ client/src/Components/RnR/Reviews/IndividualReview/Helpfulness.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Helpfulness(props) {
 
-return(<>
-<div>
-  <p>{`Helpful? Yes(${props.vote}) |`}</p>
-</div>
-</>)
+  const [voted, setVoted] = useState(false)
+
+  function voteReview(e) {
+    if (voted === true) {
+      (alert("You have voted this review."))
+    } else {
+      setVoted(true);
+      console.log(props)
+      axios.put('/reviews/:review_id/helpful', {review_id: e.target.id})
+      .then(() => {
+        console.log('Review voted')
+      })
+      .catch((err) => {
+        console.log('failed')
+      })
+    }
+  }
+
+  return(<>
+    <div>
+      <button onClick={voteReview}>{`Helpful? Yes(${props.vote}) |`}</button>
+    </div>
+  </>)
 }

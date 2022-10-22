9801fc4242271ca6ffdef48a2df9c3f9269f21dc
Blake Crawford <blake.crawford@outlook.com>
2022-10-22 11:33:30 -0700

    cleaned up more code

--- client/src/Components/RnR/Ratings/RatingsChart.jsx
+++ client/src/Components/RnR/Ratings/RatingsChart.jsx
@@ -5,9 +5,9 @@ export default function RatingsChart(props) {
 
   return(<>
     {
-      props.chart.map((rating) => {
+      props.chart.map((rating, index) => {
         return(
-          <div className="rating-bar">
+          <div className="rating-bar" key={index}>
             <p>{`${5 - props.chart.indexOf(rating)} stars: `}</p>
             <h3>{`=====${rating}=====`}</h3>
           </div>

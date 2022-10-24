c237aa4dc6425b8502a4154f49e4890363aa6616
Chuck Song <chuckthedeveloper@gmail.com>
2022-10-23 23:57:44 -0400

    Implemented review photos feature

---
+++ client/src/Components/RnR/Reviews/IndividualReview/ReviewPhotos.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import FullSizePhoto from "./FullSizePhoto.jsx";
+
+export default function ReviewPhotos(props) {
+
+  const [enlarged, setEnlarged] = useState(false);
+  const [imageURL, setImageURL] = useState("");
+
+  function zoomInImage(e) {
+    setImageURL(e.target.src)
+    setEnlarged(true)
+  }
+
+  function zoomOutImage() {
+    setEnlarged(false)
+  }
+
+  return(<span>
+    {props.photos.map((photo) => {
+      return <img src={photo.url}
+                  alt="photo"
+                  key={photo.url}
+                  className="thumbnail-photo"
+                  style={{width:"60px", height:"60px"}}
+                  onClick={zoomInImage}
+              />
+      })
+    }
+    {enlarged
+     ? <FullSizePhoto zoomOutImage={zoomOutImage}
+                      imageURL={imageURL} />
+     : null }
+  </span>)
+}

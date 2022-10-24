26c700b79af3df902bc111c0e715e4b93e7c0e6a
Chuck Song <chuckthedeveloper@gmail.com>
2022-10-24 01:16:40 -0400

    Enabled display rating stars from each review

--- client/src/Components/RnR/Reviews/IndividualReview/IndividualStars.jsx
+++ client/src/Components/RnR/Reviews/IndividualReview/IndividualStars.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import yellowStar from "../../pic/single-star.png"
 
 export default function IndividualStars(props) {
 
-  return(<>
-    <div>
-      <h3>{`${props.star} stars`}</h3>
-    </div>
-  </>)
+  let array = [];
+  for(let i = 0; i < props.star; i++) {
+    array.push(0)
+  }
+  return(<div style={{width:"100px"}}>
+  {console.log(array.length, array)}
+    {array.map((element) => {
+      {console.log("here!")}
+      return <img src={yellowStar} style={{width:"20px", height:"20px"}}/>
+    })}
+  </div>)
 }

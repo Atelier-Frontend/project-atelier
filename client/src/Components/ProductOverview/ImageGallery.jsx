baaddb47a4d49feb2d6bb5f37ab310aa08227800
Blake Crawford <blake.crawford@outlook.com>
2022-10-24 21:20:21 -0700

    expanded view now displays icons instead of images

--- client/src/Components/ProductOverview/ImageGallery.jsx
+++ client/src/Components/ProductOverview/ImageGallery.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FiArrowRight, FiArrowLeft } from 'react-icons/fi';
 import { AiOutlineExpand } from 'react-icons/ai';
 import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
+import { BsFillCircleFill } from 'react-icons/bs';
+import circle from './blue-circle.png';
 
 class ImageGallery extends React.Component {
   constructor(props) {
@@ -17,7 +19,6 @@ class ImageGallery extends React.Component {
     this.prevImage = this.prevImage.bind(this);
     this.expandImage = this.expandImage.bind(this);
     this.onClick = this.onClick.bind(this);
-    this.scroll = this.scroll.bind(this);
   }
 
   componentDidMount() {
@@ -76,24 +77,20 @@ class ImageGallery extends React.Component {
     this.setState({ current: Number(event.target.id) });
   }
 
-  scroll() {
-
-  }
-
   render() {
     return (
       <div className='image-gallery'>
         <AiOutlineExpand 
           className={this.state.expand ? 'ex-button' : 'expand-button'} 
-          onClick={this.expandImage}/>
+          onClick={this.expandImage} />
         {this.state.current !== 0 && (
           <FiArrowLeft 
             className={this.state.expand ? 'ex-left-arrow' : 'left-arrow'} 
-            onClick={this.prevImage}/>)}
+            onClick={this.prevImage} />)}
         {this.state.current < this.state.length - 1 && (
           <FiArrowRight 
             className={this.state.expand ? 'ex-right-arrow' : 'right-arrow'} 
-            onClick={this.nextImage}/>)}
+            onClick={this.nextImage} />)}
         {this.state.stylePhotos.map((style, index) => (
           <div className={index === this.state.current ? 'slide-active' : 'slide'} key={index}>
             {index === this.state.current && (
@@ -101,27 +98,36 @@ class ImageGallery extends React.Component {
                 className={this.state.expand ? 'expand-image' : 'main-image'} 
                 alt='image in carousel' 
                 key={index} 
-                src={style.url} />)}
+                src={style.url} 
+                onClick={this.expandImage} />)}
           </div>))}
         <div className={this.state.expand ? 'product-hidden' : 'sidebar-arrows'}>
           {this.state.current !== 0 && (
             <IoIosArrowUp
               className='up-arrow' 
-              onClick={this.prevImage}/>)}
+              onClick={this.prevImage} />)}
           {this.state.current < this.state.length - 1 && (
             <IoIosArrowDown
               className='down-arrow'
-              onClick={this.nextImage}/>)}
+              onClick={this.nextImage} />)}
         </div>  
-        <div className={this.state.expand ? 'product-hidden' : 'sidebar-gallery'} id='sidebar'>
+        <div className={this.state.expand ? 'transform-images' : 'sidebar-gallery'} id='sidebar'>
             {this.state.stylePhotos.map((style, index) => (
               <div className='slide'key={index}>
-                <img 
-                  className={index === this.state.current ? 'sidebar-highlight' : 'sidebar-image'} 
-                  alt='image in side carousel'
-                  id={index}
-                  src={style.thumbnail_url}
-                  onClick={this.onClick}/>
+                {this.state.expand && 
+                  <img
+                    className={index === this.state.current ? 'transform-image-highlight' : 'transformed-image'}
+                    alt='black circle icon'
+                    id={index}
+                    src={circle}
+                    onClick={this.onClick} />}
+                {this.state.expand === false &&
+                  <img 
+                    className={index === this.state.current ? 'sidebar-highlight' : 'sidebar-image'} 
+                    alt='image in side carousel'
+                    id={index}
+                    src={style.thumbnail_url}
+                    onClick={this.onClick} />}
               </div>))}
         </div> 
       </div>)}
